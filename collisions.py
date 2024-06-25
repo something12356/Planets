56a2dfeb4eb7851c7eb24ad460f6c09d93431a5d
something12356 <66644277+something12356@users.noreply.github.com>
2024-06-25 18:38:27 +0100

    Fixed coefficient of restitution with the balls

--- collisions.py
+++ collisions.py
@@ -4,7 +4,7 @@ import numpy as np
 import math as maths
 
 SIZE = [1920, 1080]
-T = 0.01
+T = 0.1
 
 x = 0
 y = 1
@@ -70,14 +70,13 @@ class ball:
         tan1,norm1, tan2,norm2 = self.vel - dot1*n,dot1, ball2.vel-dot2*n,dot2
         m1, m2 = self.size, ball2.size
         # print(m1*mag(self.vel)**2+m2*mag(ball2.vel)**2)
-        norm1New, norm2New = (norm1*(m1-m2)+2*m2*norm2)/(m1+m2), (norm2*(m2-m1)+2*m1*norm1)/(m1+m2)
+        norm1New, norm2New = (norm1*m1+norm2*m2-m2*e*(norm1-norm2))/(m1+m2), (norm1*m1+norm2*m2-m1*e*(norm2-norm1))/(m1+m2)
         self.vel = tan1+norm1New*n
         ball2.vel = tan1+norm2New*n
         # print(m1*mag(self.vel)**2+m2*mag(ball2.vel)**2)
 
-# balls = [ball(np.array([random.uniform(0.0,1920.0),random.uniform(0.0,1080.0)]),np.array([random.uniform(-5.0,10.0),random.uniform(-5.0,10.0)]),random.uniform(5,20),(random.randint(0,255),random.randint(0,255),random.randint(0,255))) for i in range(1000)]
+balls = [ball(np.array([random.uniform(0.0,1920.0),random.uniform(0.0,1080.0)]),np.array([(-1)**i*random.uniform(0.0,40.0),(-1)**i*random.uniform(0.0,40.0)]),random.uniform(9,10),(i%255,0,255-i%255)) for i in range(256)]
 # balls.append(ball(np.array([20.0,520.0]),np.array([0.0,0.0]),20,"blue"))
-balls = []
 cells = []
 
 pygame.init()
@@ -97,19 +96,30 @@ while True:
                 T += 0.01
             if event.key == pygame.K_DOWN:
                 T -= 0.01
-    if count % 3 == 0:
-        a = count % 510
-        if a > 255:
-            a = 510 - a
-        b = 255 - a
-        balls.append(ball(np.array([20.0,20.0]),np.array([30.0,0.0]),10,(a,0,b)))
+    # if count % 10 == 0:
+    #     colOffset = count % 1020
+    #     if colOffset > 510:
+    #         colOffset = 1020 - colOffset
+    #     print(colOffset)
+    #     a = 255 - colOffset
+    #     if a < 0:
+    #         a = 0
+    #     b = colOffset
+    #     if b > 255:
+    #         b = 510 - b
+    #     c = colOffset - 255
+    #     if c < 0:
+    #         c = 0
+    #     print(a,b,c)
+    #     balls.append(ball(np.array([20.0,20.0]),np.array([1200.0,0.0]),10,(a,b,c)))
+
     cells = addToCells(cells, balls)
 
     for i in range(len(cells)):
         for ball1 in cells[i]:
             pygame.draw.circle(screen,ball1.colour,ball1.pos,ball1.size)
             ball1.move()        
-            ball1.reflect(0.1)
+            ball1.reflect(1.5)
             for j in [-97,-96,-95,-1,0,1,95,96,97]:
                 if i+j > 5183 or i+j < 0:
                     continue
@@ -122,15 +132,15 @@ while True:
                         ball2.pos += 0.5*overlap*unit(ball2.pos-ball1.pos)
                         if ball1.isColliding or ball2.isColliding:
                             continue
-                        ball1.collide(ball2, 0.1)
+                        ball1.collide(ball2, 0)
                         ball1.isColliding = True
                         ball2.isColliding = True
 
     energy = 0
     for ball1 in balls:
-        ball1.vel += np.array([0.0, 6.0])
         ball1.isColliding = False
         energy += 0.5*ball1.size*mag(ball1.vel)**2
+        print(mag(ball1.vel))
     print("Total kinetic energy:",energy)
     count += 1
     # if count == 50:
