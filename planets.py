2b4f78a3424c2c9496d1b7bc8c62982725cfe3bf
Moth <randomcat695@protonmail.com>
2024-07-10 14:12:47 +0100

    Getters and setters have been implemented. Arrow weirdness have been fixed (the p2 arrow case was using the p1 mass smh)

--- planets.py
+++ planets.py
@@ -2,7 +2,7 @@ import math as maths
 import pygame
 import numpy as np
 
-## Time-scale, how much velocity and position should change per tick
+## Time-scale, how much vel and position should change per tick
 ## Lower value = slower but more accurate simulation
 T=1
 ## Gravitational constant, defines how strong gravity is
@@ -50,37 +50,63 @@ def com(planets):
     com = np.array([0.0,0.0])
     mass = 0
     for p in planets:
-        com[0] += p.mass*p.pos[0]
-        com[1] += p.mass*p.pos[1]
-        mass += p.mass
+        com[0] += p.getMass()*p.getPos()[0]
+        com[1] += p.getMass()*p.getPos()[1]
+        mass += p.getMass()
     return com/mass
 
 class planet:
-    def __init__(self,size,velocity,mass,pos,colour):
+    def __init__(self,size,vel,mass,pos,colour):
         self.size = size
-        self.velocity = velocity
+        self.vel = vel
         self.mass = mass
         self.pos = pos
         self.colour = colour
-        self.checked = False
-        self.force = 0
+        self.resultant = 0
     
+    ## All the getters and setters
+    def getPos(self):
+        return self.pos
+
+    def getMass(self):
+        return self.mass
+
+    def getVel(self):
+        return self.vel
+
+    def getResultant(self):
+        return self.resultant
+
+    def getColour(self):
+        return self.colour
+
+    def getSize(self):
+        return self.size
+
+    ## Adds a force to the resultant force on the planet
+    def addForce(self, force):
+        self.resultant += force
+
+    ## Sets velocity
+    ## Uses F = ma to find acceleration, add to vel
+    def secondLaw(self, force):
+        self.vel += T*force/self.getMass()
+    
+    ## Sets position
+    ## Add vel to position to make it move
+    def move(self, step):
+        self.pos += step
+
+    ## End of getters and setters
+
     ## Uses F = GMm/r**2 to work out the force on a planet
     ## Breaks it into components by doing F*adj/hyp, F*opp/hyp (Fcos(a) and Fsin(a))
     def gravity(self, planet2):
-        r = planet2.pos - self.pos
-        F = G*(self.mass*planet2.mass)/(mag(r)**2)
+        r = planet2.getPos() - self.getPos()
+        F = G*(self.getMass()*planet2.getMass())/(mag(r)**2)
         Fx = F*r[0]/mag(r)
         Fy = F*r[1]/mag(r)
         return np.array([Fx,Fy])
-    
-    ## Uses F = ma to find acceleration, add to velocity
-    def secondLaw(self):
-        self.velocity += T*self.force/self.mass
-    
-    ## Add velocity to position to make it move
-    def move(self):
-        self.pos += T*self.velocity
 
 ## All the planets being defined
 # sun1 = planet(30,np.array([0.0,0.0]),4000,np.array([960.0,780.0]),(0,255,255))
@@ -91,10 +117,10 @@ class planet:
 # planets = [sun1,sun2,earth]
 
 sun = planet(20,np.array([1.0,0.0]),1000000,np.array([960.0,540.0]),(255,255,0))
-venus = planet(7,np.array([-4.0,-1.0]),1,np.array([1200.0,910.1]),(139,115,85))
-earth = planet(7,np.array([4.0,-2.0]),1,np.array([960.0,270.0]),(0,0,255))
+venus = planet(7,np.array([-2.0,-1.0]),1,np.array([1200.0,910.1]),(139,115,85))
+earth = planet(7,np.array([5.0,-2.0]),1,np.array([960.0,270.0]),(0,0,255))
 # moon = planet(5,np.array([2.8690832,-0.03653574]),0.012,np.array([955.0,270]),"grey")
-jupiter = planet(10,np.array([0.0,4.0]),333000,np.array([500.0,540.0]),(210,105,30))
+jupiter = planet(10,np.array([0.0,3.0]),33300,np.array([500.0,540.0]),(210,105,30))
 planets = [sun,venus, earth, jupiter]
 
 pygame.init()
@@ -134,33 +160,33 @@ while running:
         for p2 in planets[planets.index(p1)+1:]:
             p1p2gravity = p1.gravity(p2)
             strength = mag(p1p2gravity)
-            p1.force += p1p2gravity
+            p1.addForce(p1p2gravity)
             ## Draws force arrows showing the forces acting on the planet
             if planets.index(p1) == focus and not comFocus:
-                arrowsToDraw.append(["white", p1.pos, np.copy(p1p2gravity), p1.size])
+                arrowsToDraw.append(["white", p1.getPos(), np.copy(p1p2gravity), p1.getSize()])
             if planets.index(p2) == focus and not comFocus:
-                arrowsToDraw.append(["white", p2.pos, -1*np.copy(p1p2gravity), p1.size])
+                arrowsToDraw.append(["white", p2.getPos(), -1*np.copy(p1p2gravity), p2.getSize()])
             ## Can take away here due to Newton's third law, each force has equal and opposite reaction force
-            p2.force -= p1p2gravity
+            p2.addForce(-p1p2gravity)
         if planets.index(p1) == focus and not comFocus:
-            arrowsToDraw.append([p1.colour, p1.pos, np.copy(p1.force), p1.size])
-            arrowsToDraw.append(np.copy(p1.force))
-        p1.secondLaw()
+            arrowsToDraw.append([p1.getColour(), p1.getPos(), np.copy(p1.getResultant()), p1.getSize()])
+            arrowsToDraw.append(np.copy(p1.getResultant()))
+        p1.secondLaw(p1.getResultant())
         ## Takes position before and after so that the lines for the orbits can be drawn
-        beforePos = np.copy(p1.pos)
-        p1.move()
-        afterPos = np.copy(p1.pos)
-        linesToDraw.append([beforePos,afterPos,p1.colour])
-        p1.force = 0
+        beforePos = np.copy(p1.getPos())
+        p1.move(T*p1.getVel())
+        afterPos = np.copy(p1.getPos())
+        linesToDraw.append([beforePos,afterPos,p1.getColour()])
+        p1.resultant = 0
 
         ## Makes it so that the screen follows whichever planet the user wants to look at
     ## Alternatively follows the centre of mass, useful for binary star systems
     if comFocus:
         focusDisplacement = centre - com(planets)
     else:
-        focusDisplacement = centre - planets[focus].pos
+        focusDisplacement = centre - planets[focus].getPos()
     for p in planets:
-        p.pos += focusDisplacement
+        p.move(focusDisplacement)
     for line in linesToDraw:
         line[0] += focusDisplacement
         line[1] += focusDisplacement
@@ -179,21 +205,22 @@ while running:
     if not arrows:
         arrowsToDraw = []
     else:
-        force = arrowsToDraw[-1]
+        if len(arrowsToDraw) > 0:
+            forceToDraw = arrowsToDraw[-1]
     while len(arrowsToDraw) > 1:
         arrow = arrowsToDraw.pop(0)
         if arrow[0] != "white":
-            print("RESULTANT:", maths.log(mag(arrow[2])+1,1000),"VECTOR:",arrow[2])
-            drawArrow(arrow[0], arrow[1], arrow[1]+maths.log(mag(force)+1,1000)*240*mag(arrow[2])/mag(force)*unit(arrow[2])+2*arrow[3]*unit(arrow[2]))
+            # print("RESULTANT:", abs(maths.log(mag(forceToDraw)+1,1000))*240*mag(arrow[2]), abs(arrow[3]))
+            drawArrow(arrow[0], arrow[1], arrow[1]+abs(maths.log(mag(forceToDraw)+1,1000))*240*mag(arrow[2])/mag(forceToDraw)*unit(arrow[2])+4*abs(arrow[3])*unit(arrow[2]))
         else:
-            print("COMPONENT:", maths.log(mag(arrow[2])+1,1000),"VECTOR:",arrow[2])
-            drawArrow(arrow[0], arrow[1], arrow[1]+maths.log(mag(force)+1,1000)*60*mag(arrow[2])/mag(force)*unit(arrow[2])+2*arrow[3]*unit(arrow[2]))
+            # print("COMPONENT:", abs(maths.log(mag(forceToDraw)+1,1000))*240*mag(arrow[2]), abs(arrow[3]))
+            drawArrow(arrow[0], arrow[1], arrow[1]+abs(maths.log(mag(forceToDraw)+1,1000))*60*mag(arrow[2])/mag(forceToDraw)*unit(arrow[2])+2*abs(arrow[3])*unit(arrow[2]))
     arrowsToDraw = []
 
     ## Draws all the planets
     for p in planets:       
-        pygame.draw.circle(screen,p.colour,p.pos,p.size)
-        pygame.draw.aaline(screen,(p.colour),p.pos,p.pos)
+        pygame.draw.circle(screen,p.getColour(),p.getPos(),p.getSize())
+        pygame.draw.aaline(screen,(p.getColour()),p.getPos(),p.getPos())
 
     pygame.display.flip()
     clock.tick(60)
