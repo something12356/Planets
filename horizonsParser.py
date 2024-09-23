db5594cee3a46cc531197b930a48b15c4a33b516
Moth <randomcat695@protonmail.com>
2024-09-23 11:21:46 +0100

    Figured out a way to find the line that mass is on at least, should be able to parse units (IMPORTANT SO ORDER OF MAGNITUDE IS RIGHT) and the actual number. Then just repeat for every quantity you need (position and vel might be tricky because they come up so much though)

--- horizonsParser.py
+++ horizonsParser.py
@@ -1,3 +1,7 @@
 import requests
-response = requests.get("https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='500@0'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'&CSV_FORMAT='YES'")
+response = requests.get("https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='10'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='500@0'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'&CSV_FORMAT='YES'")
 print(response.text)
+
+for i in range(len(response.text)):
+    if response.text[i:i+4] == 'Mass':
+        print(response.text[i:i+10])
