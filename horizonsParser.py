fd154048f6fa2db68bdb350750960e57ed46d238
Moth <randomcat695@protonmail.com>
2024-09-26 09:44:59 +0100

    Parsing now working, just need to integrate it with planets.

--- horizonsParser.py
+++ horizonsParser.py
@@ -1,5 +1,5 @@
 import requests
-import math as maths
+import numpy as np 
 response = requests.get("https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='VECTORS'&CENTER='500@0'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'")
 print(response.text)
 
@@ -23,10 +23,12 @@ def extractValue(text, expectingScale=False, conversionFactor=1):
     ## Once a digit is found it loops until a non-digit character is found. This string is taken to be the value.
     for i in range(scaleAt+3,40):
         if text[i].isdigit():
-            for j in range(1,15):
+            for j in range(1,40):
                 if i+j >= 40:
                     break
                     print("Parsing error")
+                ## The E, + and - being allowed is for scientific notation, because velocity and position,
+                ## unlike mass and radius, are presented in scientific notation.
                 if not text[i+j].isdigit() and text[i+j] not in ['-','.', 'E', '+']:
                     ## This loop removes error margins, not interested, just need a value
                     for k in range(len(text[i:i+j])):
@@ -39,14 +41,30 @@ def extractValue(text, expectingScale=False, conversionFactor=1):
 
     return scale*value*conversionFactor
 
+position = np.array([0.0, 0.0, 0.0])
+velocity = np.array([0.0, 0.0, 0.0])
+count = 0
 for i in range(len(response.text)):
     # Mass is in kg, so no multiplying by a factor of 10 is needed
     if response.text[i:i+6] == 'Mass x' or response.text[i:i+5] == 'Mass, ':
         mass = extractValue(response.text[i:i+40],True)
-    ## Thankfully volume is the same for both planets and the sun, so it can just be extracted the same way
+    ## Thankfully radius is the same for both planets and the sun, so it can just be extracted the same way
     if response.text[i:i+16] == 'Vol. mean radius':
-        vol = extractValue(response.text[i:i+40], False, 1000)
+        radius = extractValue(response.text[i:i+40], False, 1000)
     
-    if response.text[i:i+3] == 'X =':
-        posX = extractValue(response.text[i:i+40], False, 1000)
-        print(posX)
+    if response.text[i:i+3] in ['X =','Y =','Z =','VX=','VY=','VZ=']:
+        if count < 3:
+            position[count] = extractValue(response.text[i:i+40], False, 1000)
+            print(response.text[i:i+3])
+        else:
+            velocity[count-3] = extractValue(response.text[i:i+40], False, 1000)
+        if count == 5:
+        ## NASA provides a list of coordinates of where the planet will be over the
+        ## Next month. This is not necessary for our purposes as we just need initial
+        ## conditions and then the rest will be simualted from there, so after
+        ## the velocity is found, the loop breaks.
+            break
+        count += 1
+
+print(position)
+print(velocity)
