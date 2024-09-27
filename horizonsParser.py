ad636c3ef40f62bcf4f5e5c9f61f4d5dd69a50e8
Moth <randomcat695@protonmail.com>
2024-09-27 12:03:15 +0100

    Horizons seems to be working for all bodies now!

--- horizonsParser.py
+++ horizonsParser.py
@@ -10,7 +10,7 @@ def extractValue(text, expectingScale=False, conversionFactor=1):
     scaleAt = 0
     scale = 1
     if expectingScale:
-        for i in range(40):
+        for i in range(50):
             if text[i] == '^':
                 for j in range(1,25):
                     if not text[i+j].isdigit():
@@ -22,10 +22,9 @@ def extractValue(text, expectingScale=False, conversionFactor=1):
     ## Once a digit is found it loops until a non-digit character is found. This string is taken to be the value.
     for i in range(scaleAt+3,40):
         if text[i].isdigit():
-            for j in range(1,40):
-                if i+j >= 40:
+            for j in range(1,50):
+                if i+j >= 50:
                     break
-                    print("Parsing error")
                 ## The E, + and - being allowed is for scientific notation, because velocity and position,
                 ## unlike mass and radius, are presented in scientific notation.
                 if not text[i+j].isdigit() and text[i+j] not in ['-','.', 'E', '+']:
@@ -53,12 +52,11 @@ def getEphemeris(target):
 
     for i in range(len(response.text)):
         # Mass is in kg, so no multiplying by a factor of 10 is needed
-        if response.text[i:i+6].lower() == 'Mass x' or response.text[i:i+5] == 'Mass, ':
-            mass = extractValue(response.text[i:i+40],True)
+        if response.text[i:i+6].lower() == 'mass x' or response.text[i:i+6].lower() == 'mass, ':
+            mass = extractValue(response.text[i:i+50],True)
         ## Thankfully radius is the same for both planets and the sun, so it can just be extracted the same way
-        if response.text[i:i+16].lower() == 'Vol. mean radius':
-            size = extractValue(response.text[i:i+40], False, 1000)
-            print("HI!")
+        if response.text[i:i+16].lower() == 'vol. mean radius':
+            size = extractValue(response.text[i:i+50], False, 1000)
         
         if response.text[i:i+3] in ['X =','Y =','Z =','VX=','VY=','VZ=']:
             if count < 3:
@@ -73,7 +71,6 @@ def getEphemeris(target):
                 break
             count += 1
 
-    print(velocity)
     return size, velocity, mass, position
 
 print(getEphemeris(10))
