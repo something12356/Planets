1c8b627a8c75227a09acde50f155b020e796a7d1
something12356 <66644277+something12356@users.noreply.github.com>
2024-11-06 18:23:14 +0000

    One typo fixed

--- planets.py
+++ planets.py
@@ -353,7 +353,7 @@ while running:
     ## Alternatively, follows the centre of mass, useful for binary star systems
     # focusAdjustment(planets, comFocus)
     if comparison:
-        ## Thes2e rects get rid of old drawings, similar to doing screen.fill((0,0,0)) to refresh the display
+        ## These rects get rid of old drawings, similar to doing screen.fill((0,0,0)) to refresh the display
         pygame.draw.rect(comparisonSurface1, 'black', (0, 0, 1920, 1080))
         pygame.draw.rect(comparisonSurface2, 'black', (0, 0, 1920, 1080))
         drawPlanet(planets[planetsToCompare[0]], np.array([480, 540]), comparisonSurface1)
